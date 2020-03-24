2ee17287acfc4fd7b5e65d4b8cf335288eee83af
Adhitya Mohan <me@adhityamohan.in>
2020-03-24 12:52:42 +0530

    msm8998-common: Import new cne app

--- msm8998-common/Android.mk
+++ msm8998-common/Android.mk
@@ -158,9 +158,9 @@ LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := CNEService
+LOCAL_MODULE := CneApp
 LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/product/priv-app/CNEService/CNEService.apk
+LOCAL_SRC_FILES := proprietary/product/priv-app/CneApp/CneApp.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
